0bdf4454d6e7cc7960d57f7d5cf71546c5072bc5
Ronin11 <nate.ashby11@gmail.com>
2020-03-10 19:35:28 -0600

    15 works, but is not fast

--- 015/main.js
+++ 015/main.js
@@ -1,42 +1,27 @@
 // projecteuler.net/problem=15
-const treeMaker = () => {
-	const id = 0
-	const createNode = isEnd => ({
-		isEnd,
-		id: id++,
-		rightChild: undefined,
-		leftChild: undefined,
-		addRightChild: child => rightChild = child,
-		addLeftChild: child => leftChild = child
-	})
 
-	const createTree = size => {
-		// Generates the vertices of the graph. I.E. a size of 2 will result in a 3 x 3 graph
-		size += 1
-		const base = createNode()
-		let node = base
-		for(let i = 0; i < size; i++){
-			let newRightNode = createNode()
-			node.addRightChild(newRightNode)
-			for(let j = 0; j < size; j++){
-				let newLeftNode = createNode()
-				node.addLeftChild(newLeftNode)
-			}
-			node = newRightNode
-		}
+const traverseGrid = (size, x, y) => {
+	const xLim = x === size
+	const yLim = y === size
 
-		return base
+	
+	if(xLim && yLim){
+		return 1
 	}
 
-	return {
-		createTree
+	let paths = 0
+	if(!yLim){
+		paths +=  traverseGrid(size, x, y + 1)
 	}
+	if(!xLim){
+		paths += traverseGrid(size, x + 1, y)
+	}
+
+	return paths
 }
 
-
-const constraint = 2
-const maker = treeMaker()
-const paths = maker.createTree(constraint)
+const constraint = 20
+const paths = traverseGrid(constraint, 0, 0)
 
 console.log(`Total paths in a size ${constraint} grid: ${paths}`)
 
