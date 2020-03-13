aedb43dca9ed6f15cf6c7bc76e09a5a98ac4b6e9
Ronin11 <nate.ashby11@gmail.com>
2020-03-12 23:32:04 -0600

    #15 actually works, DP for the win

--- 015/main.js
+++ 015/main.js
@@ -1,27 +1,31 @@
 // projecteuler.net/problem=15
 
-const traverseGrid = (size, x, y) => {
-	const xLim = x === size
-	const yLim = y === size
+const buildGraph = size => {
+	const arr = []
 
-	
-	if(xLim && yLim){
-		return 1
+	for(let i = 0; i < size; i++){
+		arr.push(new Array(size))
 	}
 
-	let paths = 0
-	if(!yLim){
-		paths +=  traverseGrid(size, x, y + 1)
-	}
-	if(!xLim){
-		paths += traverseGrid(size, x + 1, y)
-	}
-
-	return paths
+	return arr
 }
 
-const constraint = 20
-const paths = traverseGrid(constraint, 0, 0)
+const binomialer = graph => {
+	for(let x = 0; x < graph.length; x++){
+		for(let y = 0; y < graph[x].length; y++){
+			if(y === 0 || x === 0){
+				graph[x][y] = 1
+			}else{
+				graph[x][y] = graph[x - 1][y] + graph[x][y - 1]
+			}
+		}
+	}
+	return graph[graph.length - 1][graph[0].length - 1]
+}
+
+const constraint = 21
+const graph = buildGraph(constraint)
+const paths = binomialer(graph)
 
 console.log(`Total paths in a size ${constraint} grid: ${paths}`)
 
